8d7ae49125843cc0b98736743f033b8f3ff15720
lukezzz <sd65745@gmail.com>
2025-06-04 10:53:20 +0800

    creating and combining views

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by luke on 2025/6/3.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
