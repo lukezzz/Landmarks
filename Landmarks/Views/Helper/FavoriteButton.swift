5a7ab75e120b0431beea290f20aca8bf8360467a
lukezzz <sd65745@gmail.com>
2025-06-07 13:41:45 +0800

    create a favoriate button for each landmark

---
+++ Landmarks/Views/Helper/FavoriteButton.swift
@@ -0,0 +1,26 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by luke on 2025/6/7.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundStyle(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
--- Landmarks/Views/Landmarks/LandmarkDetail.swift
+++ Landmarks/Views/Landmarks/LandmarkDetail.swift
@@ -8,10 +8,17 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @Environment(ModelData.self) var modelData
     
     var landmark: Landmark
     
+    var landmarkIndex: Int {
+            modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+        }
+    
     var body: some View {
+        @Bindable var modelData = modelData
+        
         ScrollView {
             MapView(coordinate: landmark.locationCoordinate)
                 .frame(height: 300)
@@ -21,11 +28,14 @@ struct LandmarkDetail: View {
                 .padding(.bottom, -130)
             
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(.title)
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
+                
                 HStack {
                     Text(landmark.park)
-                        
                     Spacer()
                     Text(landmark.state)
                         
